8ca3725aeb643ab13d94c8551172b27d10e5a865
SeokJae <75005139+seokjae88@users.noreply.github.com>
2022-04-08 21:35:50 +0900

    Time: 3 ms (99.33%), Space: 16.9 MB (6.65%) - LeetHub

--- 905-sort-array-by-parity/905-sort-array-by-parity.cpp
+++ 905-sort-array-by-parity/905-sort-array-by-parity.cpp
@@ -2,15 +2,23 @@ class Solution {
 public:
     vector<int> sortArrayByParity(vector<int>& nums) {
         deque<int> q;
+        bool isExistEven = false;
         for (int i = 0; i < nums.size(); i++) {
             if (nums[i] % 2) {
                 q.push_back(nums[i]);
             }
             else {
                 q.push_front(nums[i]);
+                isExistEven = true;
             }
         }
-        vector<int> ret(q.begin(), q.end());
-        return ret;
+        if (isExistEven) {
+            vector<int> ret(q.begin(), q.end());
+            return ret;
+        }
+        else {
+            return nums;
+        }
+        
     }
 };
