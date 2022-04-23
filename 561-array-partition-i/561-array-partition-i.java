22ab45b218edf1ba1ed1261adb1f85316f14e22a
SeokJae <75005139+seokjae88@users.noreply.github.com>
2022-04-23 19:09:10 +0900

    Time: 15 ms (59.62%), Space: 54.6 MB (20.88%) - LeetHub

--- 561-array-partition-i/561-array-partition-i.java
+++ 561-array-partition-i/561-array-partition-i.java
@@ -4,10 +4,8 @@ class Solution {
     public int arrayPairSum(int[] nums) {
         Arrays.sort(nums);
         int ret = 0;
-        for (int i=0; i<nums.length; i++) {
-            if ((i % 2) == 0) {
-               ret += nums[i];
-            }
+        for (int i=0; i<nums.length; i += 2) {
+            ret += nums[i];
         }
         return ret;
     }
